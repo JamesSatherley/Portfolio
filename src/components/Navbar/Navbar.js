fcc252a3bbcb888a5e9fc60ddeab6ab82eb749e0
James Satherley <contact@james.org.nz>
2023-08-25 02:30:18 +0000

    google analytics lol

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import './Navbar.scss';
+import useAnalyticsEventTracker from '../../useAnalyticsEventTracker';
 
 const Navbar = ({setDarkMode, darkMode}) => {
+  const gaEventTracker = useAnalyticsEventTracker('Navbar');
   return (
     <div className='nav'>
       <BsFillMoonStarsFill
-        onClick={() => setDarkMode(!darkMode)}
+        onClick={() => {
+          setDarkMode(!darkMode);
+          gaEventTracker('dark mode');
+        }}
         className="icon"
       />
       
       <a
+        onClick={()=>gaEventTracker('resume')}
         className="resume-button"
         href="https://drive.google.com/file/d/18v0QBSwEHPv7Yq9h9mKBosL6wOA64QBf/view?usp=sharing"
       >
