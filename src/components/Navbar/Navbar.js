461d00356740d6cff6f964aec5d4ffd3842efc3e
James Satherley <jamesrsatherley@gmail.com>
2024-04-18 00:49:27 +1200

    ...

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,29 +1,29 @@
-import React from 'react';
+import React from "react";
 import { BsFillMoonStarsFill } from "react-icons/bs";
-import './Navbar.scss';
-import useAnalyticsEventTracker from '../../useAnalyticsEventTracker';
+import "./Navbar.scss";
+import useAnalyticsEventTracker from "../../useAnalyticsEventTracker";
 
-const Navbar = ({setDarkMode, darkMode}) => {
-  const gaEventTracker = useAnalyticsEventTracker('Navbar');
+const Navbar = ({ setDarkMode, darkMode }) => {
+  const gaEventTracker = useAnalyticsEventTracker("Navbar");
   return (
-    <div className='nav'>
+    <div className="nav">
       <BsFillMoonStarsFill
         onClick={() => {
           setDarkMode(!darkMode);
-          gaEventTracker('dark mode');
+          gaEventTracker("dark mode");
         }}
         className="icon"
       />
-      
+
       <a
-        onClick={()=>gaEventTracker('resume')}
+        onClick={() => gaEventTracker("resume")}
         className="resume-button"
         href="https://drive.google.com/file/d/18v0QBSwEHPv7Yq9h9mKBosL6wOA64QBf/view?usp=sharing"
       >
         Resume
       </a>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
